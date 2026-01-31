05ec7411419e171d8a0a4f3f2fd1920a61bb4882
dipeshpadole786 <sudhirpadole00@gmail.com>
2026-01-31 08:08:14 +0530

    add backend server api

--- Frontend/src/api/api.jsx
+++ Frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://null-pointer-psid-3-12-sdg-3.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
